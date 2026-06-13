2117e993d098d0fd26ea44c5fc1f69dddec61180
Max <maximo.aguero@mi.unc.edu.ar>
2026-06-13 16:53:00 -0300

    correcciones ej1a y ej1b

--- herramientas_estimaciones.py
+++ herramientas_estimaciones.py
@@ -1,30 +1,37 @@
 from random import random
 import numpy as np
-
+from simulacion_centro_ej1 import programa
 # corregir: el programa va a devolver un struct
+
+
 def media_muestral_x_b(programa, d, T):
     res = programa(T)
-    media = np.mean(res['t_imagen'])
+    media = np.mean(res[6])
     scuad, n = 0, 1
-    while n <= 100 or np.sqrt(scuad / n) > d:
+    while n < 100 or np.sqrt(scuad / n) > d:
         n += 1
         res = programa(T)
-        nueva = np.mean(res['t_imagen'])
+        nueva = np.mean(res[6])
         mediaAnt = media
-        media = mediaAnt + ( nueva - mediaAnt) / n
-        scuad = scuad * ( 1 - 1(n-1)) + n * (media - mediaAnt) ** 2
+        media = mediaAnt + (nueva - mediaAnt) / n
+        scuad = scuad * (1 - 1 / (n-1)) + n * (media - mediaAnt) ** 2
 
     return media
 
+
 def estimador_p(programa, d, T):
-    p, n = 0 , 0
-    while n <= 100 or np.sqrt(p * ( 1 - p ) / n ) > d:
+    p, n = 0, 0
+    while n <= 100 or np.sqrt(p * (1 - p) / n) > d:
         n += 1
         res = programa(T)
-        temp = np.array(res['t_salidas'])
+        temp = np.array(res[3])
         X = np.sum(temp > T)
         if X > 0:
             X = 1
         p = p + (X - p) / n
 
     return p
+
+
+media = media_muestral_x_b(programa, 0.1, 100)
+print(media)
--- simulacion_centro_ej1.py
+++ simulacion_centro_ej1.py
@@ -1,10 +1,12 @@
 from random import random
 import numpy as np
 
-def lambda_t(t):
-    return 8 + 4 * np.sin( np.pi * t / 12 )
 
-def generar_llegada_poisson(t_actual,T_max):
+def lambda_t(t):
+    return 8 + 4 * np.sin(np.pi * t / 12)
+
+
+def generar_llegada_poisson(t_actual, T_max):
     lambda_max = 12
     t_aux = t_actual
     while True:
@@ -17,7 +19,9 @@ def generar_llegada_poisson(t_actual,T_max):
             return t_aux
 
 # fijarse lo del T, si corresponde
-def programa(T= 16):
+
+
+def programa(T=16):
     t, NA, ND = 0, 0, 0
     n1, n2 = 0, 0
     t0 = generar_llegada_poisson(t, T)
@@ -25,12 +29,15 @@ def programa(T= 16):
     t1, t2 = float('inf'), float('inf')
 
     t_llegadas = []
-    reg_n1 = [0]
-    t_atencion = []
-    t_salida_atencion = []
-    t_imagen = []
+    t_recepcion = []
+    t_validacion = []
+    t_salida_recepcion = []
     t_salidas = []
+    reg_n1 = [0]
     reg_n2 = [0]
+    # Para registrar un tiempo total en sistema
+    t_servicio_sistema = []
+    t_llegada_sistema = []
 
     while tA < float('inf') or t1 < float('inf') or t2 < float('inf'):
         evento = min(tA, t1, t2)
@@ -40,59 +47,59 @@ def programa(T= 16):
             NA += 1
             n1 += 1
             t_llegadas.append(t)
+            t_llegada_sistema.append(t)
             tA = generar_llegada_poisson(t, T)
             reg_n1.append(n1)
             if tA > T:
                 tA = float('inf')
             if n1 == 1:
-                # corregir aca de acuerdo al enunciado
-                # Y1 es el tiempo de atencion en admision
-                Y1 = np.random.exponential( 1 / 15)
+                Y1 = np.random.exponential(1 / 4)
                 t1 = t + Y1
 
+        # Fin de servicio en Recepción
         elif evento == t1:
             t = t1
             n1 -= 1
-            t_salida_atencion.append(t)
+            t_salida_recepcion.append(t)
             primero = t_llegadas.pop(0)
-            t_atencion.append( t - primero)
+            t_recepcion.append(t - primero)
             reg_n1.append(n1)
-            if n1 = 0:
+            if n1 == 0:
                 t1 = float('inf')
             else:
-                # corregir acá
-                Y1 = np.random.exponential( 1 / 15)
+                Y1 = np.random.exponential(1 / 4)
                 t1 = t + Y1
             n2 += 1
             reg_n2.append(n2)
             if n2 == 1:
-                # corregir acá
-                Y2 = np.random.exponential( 1 / 12)
+                Y2 = np.random.exponential(1 / 6)
                 t2 = t + Y2
 
-
-# fin de servicio en imagen
+        # Fin de servicio en Validación
         else:
             t = t2
             ND += 1
             n2 -= 1
-            primero = t_salida_atencion.pop(0)
-            t_imagen.append( t - primero)
+            primero = t_salida_recepcion.pop(0)
+            t_validacion.append(t - primero)
             t_salidas.append(t)
             reg_n1.append(n1)
+
+            # Registramos el tiempo total en el sistema
+            llegada = t_llegada_sistema.pop(0)
+            t_servicio_sistema.append(t - llegada)
             if n2 == 0:
                 t2 = float('inf')
             else:
-                # corregir acá
-                Y2 = np.random.exponential( 1 / 12)
+                Y2 = np.random.exponential(1 / 6)
                 t2 = t + Y2
 
     return (
         t_llegadas,
-        t_atencion,
-        t_imagen,
+        t_recepcion,
+        t_validacion,
         t_salidas,
         reg_n1,
-        reg_n2
+        reg_n2,
+        t_servicio_sistema
     )
-
