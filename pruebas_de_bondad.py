133c48fc30140a70b2dedbd3113447b073599147
Max <maximo.aguero@mi.unc.edu.ar>
2026-06-13 23:40:42 -0300

    upsi2222!

--- pruebas_de_bondad.py
+++ pruebas_de_bondad.py
@@ -2,6 +2,18 @@ import numpy as np
 import scipy.stats as stats
 
 
+def calc_esp(dist, params, limites, N_tot, obs_arr):
+    p_i = np.diff(dist.cdf(limites, *params))
+
+    p_i[0] += dist.cdf(limites[0], *params)
+    p_i[-1] += (1.0 - dist.cdf(limites[-1], *params))
+
+    esp = p_i * N_tot
+    esp = esp * (np.sum(obs_arr) / np.sum(esp))
+
+    return esp
+
+
 def prueba_bondad_ajuste(tiempos_permanencia):
     datos = np.array(tiempos_permanencia)
     N = len(datos)
@@ -22,17 +34,6 @@ def prueba_bondad_ajuste(tiempos_permanencia):
 
     obs, bins = np.histogram(datos, bins=50)
 
-    def calc_esp(dist, params, limites, N_tot, obs_arr):
-        p_i = np.diff(dist.cdf(limites, *params))
-
-        p_i[0] += dist.cdf(limites[0], *params)
-        p_i[-1] += (1.0 - dist.cdf(limites[-1], *params))
-
-        esp = p_i * N_tot
-        esp = esp * (np.sum(obs_arr) / np.sum(esp))
-
-        return esp
-
     esp_exp = calc_esp(stats.expon, (loc_exp, scale_exp), bins, N, obs)
     esp_log = calc_esp(
         stats.lognorm, (shape_log, loc_log, scale_log), bins, N, obs)
@@ -41,5 +42,3 @@ def prueba_bondad_ajuste(tiempos_permanencia):
     _, pval_log = stats.chisquare(f_obs=obs, f_exp=esp_log, ddof=2)
 
     return pval_exp, pval_log
-
-
